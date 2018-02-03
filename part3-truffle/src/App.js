3f5e9ee88ecbf6847752dc6525188d9f56fe4374
Erbil Eren <erbilleren@gmail.com>
2018-02-04 00:59:19 +0300

    part3-get and set value

--- part3-truffle/src/App.js
+++ part3-truffle/src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react'
-import SimpleContract from '../build/contracts/SimpleContract.json'
-import getWeb3 from './utils/getWeb3'
+import React, {Component} from 'react'
+import contract from 'truffle-contract'
+
+import instantiateContract from './utils/instantiateContract'
 
 import './App.css'
 
@@ -10,58 +11,15 @@ class App extends Component {
 
     this.state = {
       dataValue: 0,
-      web3: null,
       valueFromChain: 0
     }
+
   }
 
   componentWillMount() {
-    getWeb3
-    .then(results => {
-      this.setState({
-        web3: results.web3
-      })
+    // Get network provider and web3 instance. See utils/getWeb3 for more info.
 
-      // Instantiate contract once web3 provided.
-      //this.instantiateContract()
-    })
-    .catch(() => {
-      console.log('Error finding web3.')
-    })
-  }
-
-  instantiateContract(inputValue) {
-    /*
-     * SMART CONTRACT EXAMPLE
-     *
-     * Normally these functions would be called in the context of a
-     * state management library, but for convenience I've placed them here.
-     */
-
-    const contract = require('truffle-contract')
-    const simpleContract = contract(SimpleContract)
-    simpleContract.setProvider(this.state.web3.currentProvider)
-
-    // Declaring this for later so we can chain functions on simpleContract.
-    var simpleContractInstance
-
-    // Get accounts.
-    this.state.web3.eth.getAccounts((error, accounts) => {
-      simpleContract.deployed().then((instance) => {
-        simpleContractInstance = instance
-
-        // Stores a given value, 5 by default.
-        return simpleContractInstance.setData(inputValue, {from: accounts[0]})
-      }).then((result) => {
-        // Get the value from the contract to prove it worked.
-        return simpleContractInstance.getData.call({from: accounts[0]})
-      }).then((result) => {
-        // Update state with the result.
-        return this.setState({ valueFromChain: result.c[0] })
-        }).catch((error) => {
-          console.error(error);
-      })
-    })
+    instantiateContract.setWeb3()
   }
 
   handleChange(e) {
@@ -73,7 +31,17 @@ class App extends Component {
   handleClick(e) {
     e.preventDefault()
 
-    this.instantiateContract(this.state.dataValue)
+    instantiateContract.setValue(this.state.dataValue)
+  }
+
+  handleSecondClick(e) {
+    e.preventDefault()
+    
+    instantiateContract.getValue().then((result) => {
+      this.setState({
+        valueFromChain: result
+      })
+    })
   }
 
   render() {
@@ -83,8 +51,21 @@ class App extends Component {
           <div className="pure-g">
             <div className="pure-u-1-1">
               <h2>Smart Contract Example Is Working!</h2>
-              <input type="number" onChange={this.handleChange.bind(this)} />
-              <button type="submit" onClick={this.handleClick.bind(this)}>Update value</button>
+              <input
+                type="number"
+                onChange={this
+                .handleChange
+                .bind(this)}/>
+              <button
+                type="submit"
+                onClick={this
+                .handleClick
+                .bind(this)}>Update value</button>
+              <button
+                type="submit"
+                onClick={this
+                .handleSecondClick
+                .bind(this)}>Get value</button>
               <p>The value is: {this.state.valueFromChain}</p>
             </div>
           </div>
---
+++ part3-truffle/src/utils/instantiateContract.js
@@ -0,0 +1,67 @@
+import SimpleContract from '../../build/contracts/SimpleContract.json'
+import getWeb3 from './getWeb3'
+
+import contract from 'truffle-contract'
+
+class instantiateContract {
+    web3 = {}
+
+    static setWeb3() {
+        getWeb3
+            .then(results => {
+            this.web3 = results.web3
+        })
+            .catch(() => {
+                console.log('Error finding web3.')
+            })
+    }
+
+    static setValue(val) {
+        var simpleContract = contract(SimpleContract)
+        simpleContract.setProvider(this.web3.currentProvider)
+
+        this
+            .web3
+            .eth
+            .getAccounts((error, accounts) => {
+                simpleContract
+                    .deployed()
+                    .then((instance) => {
+                        // Stores a given value, 5 by default.
+                        return instance.setData(val, {from: accounts[0]})
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                    })
+            })
+    }
+
+    static getValue() {
+        return new Promise((resolve, reject) => {
+            var simpleContract = contract(SimpleContract)
+            simpleContract.setProvider(this.web3.currentProvider)
+
+            this
+                .web3
+                .eth
+                .getAccounts((error, accounts) => {
+                    simpleContract
+                        .deployed()
+                        .then((instance) => {
+                            // Get the value from the contract to prove it worked.
+                            return instance
+                                .getData
+                                .call({from: accounts[0]})
+                        })
+                        .then((result) => {
+                            // Update state with the result.
+                            resolve(result.c[0])
+                        })
+                        .catch((error) => {
+                            console.error(error);
+                        })
+                })
+        })
+    }
+}
+export default instantiateContract
