78c80ee531fb5ad603f13450939a89f00c9fbd39
Erbil Eren <erbilleren@gmail.com>
2018-02-03 15:26:32 +0300

    commit for part2 tutorial

---
+++ README.md
@@ -0,0 +1,6 @@
+NOTE: Don't forget npm install before run the project!
+
+Tutorials:
+- Part 1 (react): http://www.erbileren.com/2018/02/02/create-a-react-app-from-scratch/
+- Part 2 (webpack): soon.
+- Part 3 (truffle): soon.
---
+++ part2-webpack/src/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+ 
+class App extends Component {
+    render () {
+        return (
+            <p>This is my new react app with webpack!</p>
+        )
+    }
+}
+ 
+export default App
