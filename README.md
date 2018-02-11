778f51a60ae013a25ddde09b0437d1952f71755f
Erbil Eren <erbilleren@gmail.com>
2018-02-11 21:58:24 +0300

    blog post commit for part 3

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ NOTE: Don't forget npm install before run the project!
 Tutorials:
 - Part 1 (react): http://www.erbileren.com/2018/02/02/create-a-react-app-from-scratch/
 - Part 2 (react with webpack): http://www.erbileren.com/2018/02/07/react-app-with-webpack/
-- Part 3 (react, webpack with truffle): soon.
+- Part 3 (react, webpack with truffle): http://www.erbileren.com/2018/02/11/truffle-with-react/
--- part3-truffle/src/App.js
+++ part3-truffle/src/App.js
@@ -1,75 +1,58 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 
-import instantiateContract from './utils/instantiateContract'
-
-import './App.css'
+import instantiateContract from "./utils/instantiateContract";
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       dataValue: 0,
       valueFromChain: 0
-    }
-
+    };
   }
 
   componentWillMount() {
-    instantiateContract.setWeb3()
+    instantiateContract.setWeb3();
   }
 
   handleChange(e) {
-    e.preventDefault()
+    e.preventDefault();
 
-    this.setState({dataValue: e.target.value})
+    this.setState({ dataValue: e.target.value });
   }
 
   handleClick(e) {
-    e.preventDefault()
+    e.preventDefault();
 
-    instantiateContract.setValue(this.state.dataValue)
+    instantiateContract.setValue(this.state.dataValue);
   }
 
   handleSecondClick(e) {
-    e.preventDefault()
+    e.preventDefault();
 
-    instantiateContract.getValue().then((result) => {
+    instantiateContract.getValue().then(result => {
       this.setState({
         valueFromChain: result
-      })
-    })
+      });
+    });
   }
 
   render() {
     return (
       <div className="App">
-        <main className="container">
-          <div className="pure-g">
-            <div className="pure-u-1-1">
-              <h2>Smart Contract Example Is Working!</h2>
-              <input
-                type="number"
-                onChange={this
-                .handleChange
-                .bind(this)}/>
-              <button
-                type="submit"
-                onClick={this
-                .handleClick
-                .bind(this)}>Update value</button>
-              <button
-                type="submit"
-                onClick={this
-                .handleSecondClick
-                .bind(this)}>Get value</button>
-              <p>The value is: {this.state.valueFromChain}</p>
-            </div>
-          </div>
-        </main>
+        <h2>Smart Contract Example Is Working!</h2>
+        <input type="number" onChange={this.handleChange.bind(this)} />
+        <button type="submit" onClick={this.handleClick.bind(this)}>
+          Update value
+        </button>
+        <button type="submit" onClick={this.handleSecondClick.bind(this)}>
+          Get value
+        </button>
+        <p>The value is: {this.state.valueFromChain}</p>
       </div>
     );
   }
 }
 
-export default App
+export default App;
